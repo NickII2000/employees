6f7296a46ddea62922274d8a6e8cd055b450b7aa
Nick <Nick_II2000@mail.ru>
2022-12-17 19:22:42 +0600

    2-th commit

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -3,7 +3,7 @@ import './app-info.css';
 const AppInfo = ({ increased, employees, rised }) => {
     return (
         <div className="app-info">
-            <h1>Учёт сотрудников в компании "Добрый вечер"</h1>
+            <h1>Учёт сотрудников в компании "Доброе утро!"</h1>
             <h2>Общее число сотрудников: {employees}</h2>
             <h2>Премию получат: {increased} <i> (выделены золотым шрифтом)</i></h2>
             <h2>Идут на повышение: {rised} <i> (со звездочкой)</i></h2>
